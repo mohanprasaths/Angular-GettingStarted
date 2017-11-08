18f6f17e8e2b7318db675fd092dd0ac16e09cb16
mohanprasaths <mohanprasath@live.com>
2017-11-08 14:23:08 +0530

    basic angular seed

--- APM-Start/script.js
+++ APM-Start/script.js
@@ -1,17 +1,3 @@
-// Code goes here
-(function() {
-  var createWorker = function() {
-    return {
-      job1: function() {
-        console.log("job1")
-      },
-      job2: function() {
-        console.log("job2")
-      }
-    }
-  }
-
-  var worker = createWorker()
-  worker.job1()
-  worker.job2()
-}())
+var mainController = function($scope) {
+  $scope.message = "hellow";
+}
