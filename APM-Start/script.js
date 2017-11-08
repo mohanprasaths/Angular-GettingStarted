d1cf52e39d3f799728f5835053342b88bcec1ddf
mohanprasaths <mohanprasath@live.com>
2017-11-08 12:15:54 +0530

    removing globlas and using Immediatly invoking function (IIF's)

--- APM-Start/script.js
+++ APM-Start/script.js
@@ -1,12 +1,17 @@
 // Code goes here
-
-var createWorker = function(){
-  return {
-    job1: function(){console.log("job1")},
-    job2: function(){console.log("job2")}
+(function() {
+  var createWorker = function() {
+    return {
+      job1: function() {
+        console.log("job1")
+      },
+      job2: function() {
+        console.log("job2")
+      }
+    }
   }
-}
 
-var worker = createWorker()
-worker.job1()
-worker.job2()
+  var worker = createWorker()
+  worker.job1()
+  worker.job2()
+}())
